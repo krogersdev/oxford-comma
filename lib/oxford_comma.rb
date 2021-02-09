93c62d4b12273d5d05f6871dfa834077d693523d
Kevin <kevinarogers1@gmail.com>
2021-02-09 17:19:40 -0500

    commit tree 3f04c063ca45e0b0abd82ea07be2434d81abc6d6

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+   if array.length <= 2  
+      return array.join (" and ")
+   else 
+      array.insert(array.length-1, "and #{array[array.length - 1]}")
+        array.pop()
+        multiple_element_string = array.join(", ")
+        return multiple_element_string
+  
+   end
 end
+
